9c1506a22eda39df732802d07adf2019744e647a
Pawel Wujczyk <pwujczyk@hotmail.com>
2020-05-12 21:53:32 +0200

    add login and reference to tfs libary

---
+++ ProductivityTools.AzureDevOps.TimeTracking.Facade/TimeTrackingTFS.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.Services.Common;
+using Microsoft.VisualStudio.Services.WebApi;
+using System;
+
+namespace ProductivityTools.AzureDevOps.TimeTracking.Facade
+{
+    public class TFS
+    {
+
+        private void LoginWithPersonalAccessToken(string tfsAddress, string personalAccessToken)
+        {
+            VssConnection connection = new VssConnection(new Uri(tfsAddress), new VssBasicCredential(string.Empty, personalAccessToken));
+        }
+    }
+}
