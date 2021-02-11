99a7683ae542c8c88219c36e5674168b7f38c5b2
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-02-11 12:34:16 +0100

    new cmdleT

---
+++ EcoVadis.AzureDevOps/AddFTRemoval/AddFTRemovalCmdlet.cs
@@ -0,0 +1,26 @@
+﻿using EcoVadis.AzureDevOps.AddFTRemoval.Commands;
+using EcoVadis.AzureDevOps.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Management.Automation;
+using System.Text;
+
+namespace EcoVadis.AzureDevOps.AddFTRemoval
+{
+    [Cmdlet(VerbsCommon.Add, "FTRemoval")]
+    [Description("Add user stories for all FT which exists on production")]
+    class AddFTRemovalCmdlet : CmdletBase
+    {
+        public AddFTRemovalCmdlet()
+        {
+        }
+
+        protected override void BeginProcessing()
+        {
+            this.AddCommand(new Add(this));
+            this.ProcessCommands();
+            base.BeginProcessing();
+        }
+    }
+}
---
+++ EcoVadis.AzureDevOps/AddFTRemoval/Commands/Add.cs
@@ -0,0 +1,21 @@
+﻿using EcoVadis.AzureDevOps.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EcoVadis.AzureDevOps.AddFTRemoval.Commands
+{
+    class Add : CommandBase<AddFTRemovalCmdlet>
+    {
+        public Add(AddFTRemovalCmdlet cmdlet) : base(cmdlet)
+        {
+        }
+
+        protected override bool Condition => true;
+
+        protected override void Invoke()
+        {
+            this.Cmdlet.WriteVerbose("Hello from Add FT Removal");
+        }
+    }
+}
